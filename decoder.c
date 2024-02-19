02055892ec23ea7ef41c78732df678ee540817d1
Drake <drakec1997@gmail.com>
2024-02-19 02:52:03 -0800

    Added encoder and cleaned up formatting

--- decoder.c
+++ decoder.c
@@ -3,11 +3,10 @@
 int main() {
 
     // Data to parse
-    unsigned char source[9] = { 0xd8, 0xff, 0xe0, 0xff, 0x10, 0x00, 0x46, 0x4a, 0x46 };
+    unsigned char source[3] = { 0xb5, 0xeb, 0x2d };
 
     // Buffer for data processing
-    char buffer[4] = { 0, 0, 0, 0 };
-
+    char buffer[4] = { 0 };
 
     // Dynamically sets loop end based on data size
     int source_length = sizeof(source);
@@ -16,7 +15,7 @@ int main() {
     for (int i = 0; i < source_length; ++i) {
         printf("0x%02x ", source[i]);
     }
-    printf("==>\n");
+    printf("==> ");
 
     // Selects 3 8-byte chunks at a time to turn into 4 6-byte chunks
     for (int i = 0; i < source_length; i += 3) {
@@ -27,35 +26,35 @@ int main() {
 
         // Divides 3 8-byte chunks into 4 6-byte buffer slots
         // 0b'xxxx'xxxx ==> 0b'00xx'xxxx        
-        buffer[0] =     byte0 >> 2;
+        buffer[0]   = byte0 >> 2;
         
         // 0x03 = 0b`0000`00xx ==> 0b'00xx'0000
         // 0xf0 = 0b`xxxx`0000 ==> 0b'00xx'xxxx
-        buffer[1] =     (byte0 & 0x03) << 4;
-        buffer[1] |=    (byte1 & 0xf0) >> 4;
+        buffer[1]   =  (byte0 & 0x03) << 4;
+        buffer[1]   |= (byte1 & 0xf0) >> 4;
 
         // 0x0f = 0b'0000'xxxx ==> 0b'00xx'xx00
         // 0xc0 = 0b'xx00'0000 ==> 0b'00xx'xxxx
-        buffer[2] =     (byte1 & 0x0f) << 2;
-        buffer[2] |=    (byte2 & 0xc0) >> 6;
+        buffer[2]   =  (byte1 & 0x0f) << 2;
+        buffer[2]   |= (byte2 & 0xc0) >> 6;
 
         // 0x3f = 0b'00xx'xxxx
-        buffer[3] =     byte2 & 0x3f;
+        buffer[3]   = byte2 & 0x3f;
 
         for (int b = 0; b < 4; b++) {
             if (buffer[b] < 26) {
                 // Prints value 0-25, an uppercase letter
-                printf("%c == %i\n", 'A' + buffer[b], buffer[b]);
+                printf("%c", 'A' + buffer[b]);
             } else if (buffer[b] < 52) {
-                // Prints value 25-51, a lowercase letter
-                printf("%c == %i\n", 'a' + (buffer[b] - 26), buffer[b]);
+                // Prints value 26-51, a lowercase letter
+                printf("%c", 'a' + (buffer[b] - 26));
             } else if (buffer[b] < 62) {
                 // Prints value 52-61, a digit
-                printf("%c == %i\n", '0' + (buffer[b] - 52), buffer[b]);
+                printf("%c", '0' + (buffer[b] - 52));
             } else if (buffer[b] == 62) {
-                printf("+ == %i\n", buffer[b]);
+                printf("+", buffer[b]);
             } else if (buffer[b] == 63) {
-                printf("/ == %i\n", buffer[b]);
+                printf("/", buffer[b]);
             } else {
                 // Should never run
                 printf("\n\n Error! Bad 6-bit value: %c\n", buffer[b]);
--- encoder.c
+++ encoder.c
@@ -1,8 +1,69 @@
 #include <stdio.h>
+#include <limits.h>
 
 int main() {
     // String to encode
-    char data[] = "2P/g/xAARkpG";
-
+    unsigned char data[] = "test";
     
+    // Buffer for byte transformation
+    unsigned char buffer[3] = { 0 };
+
+    // Dynamically gets string length
+    char data_length = sizeof(data);
+
+    // Prints string
+    printf("\n");
+    for (int i = 0; i < data_length - 1; ++i) {
+        printf("%c", data[i]);
+    }
+    printf(" ==> ");
+    
+    // Convert each character from ASCII to their base-64 value
+    // +   = 43
+    // /   = 47
+    // 0-9 = 48-57
+    // A-Z = 65-90
+    // a-z = 97-120
+    for (int i = 0; i < data_length - 1; ++i) {
+        // +
+        if (data[i] == 43) {
+            data[i] = 62;
+        // /
+        } else if (data[i] == 47) {
+            data[i] = 63;
+        // 0 - 9
+        } else if (data[i] >= 48 && data[i] <= 57) {
+            data[i] += 4;
+        // A - Z
+        } else if (data[i] >= 65 && data[i] <= 90) {
+            data[i] -= 65;
+        // a - z
+        } else if (data[i] > 97 && data[i] <= 123) {
+            data[i] -= 71;
+        }
+    }
+    
+    for (int i = 0; i < data_length - 1; i += 4) {
+        // Our 6-bit chunks
+        unsigned char byte0 = data[i];
+        unsigned char byte1 = data[i + 1];
+        unsigned char byte2 = data[i + 2];
+        unsigned char byte3 = data[i + 3];
+
+        // Shifts bits to create 8-bit chunks
+        buffer[0]    = byte0 << 2;
+        buffer[0]   |= byte1 >> 4;
+
+        buffer[1]    = byte1 << 4;
+        buffer[1]   |= byte2 >> 2;
+
+        buffer[2]    = byte2 << 6;
+        buffer[2]   |= byte3;
+
+        for (int b = 0; b < 3; ++b) {
+            printf("0x%02hhx ", buffer[b]);
+        }
+    }
+    printf("\n");
+
 }
